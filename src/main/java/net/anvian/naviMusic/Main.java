491805221e590dcc8c1c363b9580ce6496f55c24
andre-carbajal <carbajalvargasandre@gmail.com>
2023-12-09 18:09:38 -0500

    fix nogui

--- src/main/java/net/anvian/naviMusic/Main.java
+++ src/main/java/net/anvian/naviMusic/Main.java
@@ -8,11 +8,21 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        boolean isGuiVisible = args.length == 0 || !args[0].equals("nogui");
-        ConsoleGUI consoleGUI = new ConsoleGUI();
-        consoleGUI.setVisible(isGuiVisible);
+        if (args.length > 0 && args[0].equals("nogui")) {
+            System.setProperty("java.awt.headless", "true");
+        }
+
+        if (!GraphicsEnvironment.isHeadless()) {
+            boolean isGuiVisible = args.length == 0 || !args[0].equals("nogui");
+            ConsoleGUI consoleGUI = new ConsoleGUI();
+            consoleGUI.setVisible(isGuiVisible);
+            System.out.println("Running in headless mode");
+        }else
+            System.out.println("Not running in headless mode");
 
         Dotenv dotenv = Dotenv.configure().load();
 
