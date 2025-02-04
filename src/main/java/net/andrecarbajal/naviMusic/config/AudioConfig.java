924925eb22e8cd74b04b4e89311e11aada8d3800
andre-carbajal <carbajalvargasandre@gmail.com>
2025-02-04 15:09:43 -0500

    Android source manager removed

--- src/main/java/net/andrecarbajal/naviMusic/config/AudioConfig.java
+++ src/main/java/net/andrecarbajal/naviMusic/config/AudioConfig.java
@@ -46,7 +46,6 @@ public class AudioConfig extends ListenerAdapter {
                 new AndroidVrWithThumbnail(),
                 new WebWithThumbnail(),
                 new WebEmbeddedWithThumbnail(),
-                new Android(),
                 new IosWithThumbnail()
                 );
 
